7a885ae1dc32d6817df5397d16209854d4fda678
iwannakom <iwannakomitopoulou@gmail.com>
2025-04-03 20:55:46 +0300

    Add files via upload

---
+++ check if anagram-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+
+        s_count, t_count = {} ,{}
+
+        for i in range (len(s)):
+            s_count = 1+ s_count.get(s[i],0)
+            t_count = 1+ t_count.get(s[i],0)
+
+        return s_count == t_count
+
+        
+        
