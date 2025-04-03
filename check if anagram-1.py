9c39f07e1265afd819cf587e22f0127de3b3bf31
iwannakom <iwannakomitopoulou@gmail.com>
2025-04-03 20:56:18 +0300

    Update check if anagram-1.py

--- check if anagram-1.py
+++ check if anagram-1.py
@@ -1,16 +1,15 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+def isAnagram(self, s: str, t: str) -> bool:
         
-        if len(s) != len(t):
-            return False
+    if len(s) != len(t):
+        return False
 
-        s_count, t_count = {} ,{}
+    s_count, t_count = {} ,{}
 
-        for i in range (len(s)):
-            s_count = 1+ s_count.get(s[i],0)
-            t_count = 1+ t_count.get(s[i],0)
+    for i in range (len(s)):
+        s_count = 1+ s_count.get(s[i],0)
+        t_count = 1+ t_count.get(s[i],0)
 
-        return s_count == t_count
+    return s_count == t_count
 
         
         
